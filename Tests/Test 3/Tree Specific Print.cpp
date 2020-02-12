7b58abc23ee858632da3ce7c127b6e6d2d480c4c
Kaloyan Nikolov <60822878+Kaloyan-Nikolov@users.noreply.github.com>
2020-02-12 16:43:24 +0200

    Create  Tree Specific Print.cpp

---
+++ Tests/Test 3/Tree Specific Print.cpp
@@ -0,0 +1,71 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+struct Node{
+    Node *left;
+    Node *right;
+    int value;
+    Node(int value){
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+class BST{
+
+public:
+    BST(){
+        root = NULL;
+    }
+    void insert(int value){
+        root = insert(root, value);
+    }
+
+	void printSpecific(int dividedBy){
+        printRec(root, dividedBy);
+    }
+
+private:
+	void printRec(Node* root, int dividedBy)
+    {
+        if(root == nullptr)
+            return;
+        
+        bool isNotLeaf = root->left || root->right;
+        if(isNotLeaf && root->value % dividedBy == 0)
+            cout << root->value << " ";
+        printRec(root->left, dividedBy);
+        printRec(root->right, dividedBy);
+    }
+
+    Node* root;
+
+    Node* insert(Node *curNode, int value){
+        if(curNode == NULL){
+            curNode =  new Node(value);
+        }else if(curNode->value < value){
+            curNode->right = insert(curNode->right, value);
+        }else if(curNode->value > value){
+            curNode->left = insert(curNode->left, value);
+        }else {
+        	//if we already have this value at the tree - do nothing
+        }
+        return curNode;
+    }
+};
+int main() {
+    int n;
+  	cin >> n;
+  	int value;
+  	BST tree;
+  	for(int i = 0 ; i < n; i++){
+      cin >> value;
+      tree.insert(value);
+    }
+  	int dividedBy;
+  	cin >> dividedBy;
+  	tree.printSpecific(dividedBy);
+    return 0;
+}
