8f180b0eb06d97e962808ae82b6fce0c7f6d8d64
Kaloyan Nikolov <60822878+Kaloyan-Nikolov@users.noreply.github.com>
2020-02-16 14:56:30 +0200

    Create Merge Sort 1.cpp

---
+++ Sorting Algorithms/Merge Sort 1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int inversions = 0;
+void merge(vector<int>& v, int l, int m, int r)
+{
+	int s1 = m - l + 1;
+	int s2 = r - m;
+
+	vector<int> L(s1);
+	vector<int> R(s2);
+
+	for (int i = 0; i < s1; i++) L[i] = v[i + l];
+	for (int i = 0; i < s2; i++) R[i] = v[i + m + 1];
+
+	int i = 0; int j = 0; int k = l;
+
+	while (i < s1 && j < s2)
+		if (L[i] < R[j])
+		{
+			v[k++] = L[i++];
+		}
+		else
+		{
+			v[k++] = R[j++];
+			inversions += s1 - i;
+		}
+
+	while (i < s1) v[k++] = L[i++];
+	while (j < s2) v[k++] = R[j++];
+}
+
+void ms(vector<int>& v, int l, int r)
+{
+	if (l < r)
+	{
+		int mid = l + (r - l) / 2;
+		ms(v, l, mid);
+		ms(v, mid + 1, r);
+		merge(v, l, mid, r);
+	}
+}
+
+int main()
+{
+	vector<int> test = { 5, 8, -3, 0, 2, 4, 1 };
+	//vector<int> test2 = { 2, 4, 0, -1 };
+	ms(test, 0, test.size() - 1);
+	for (auto i : test) cout << i << " ";
+	cout << endl << inversions << endl;
+
+	return 0;
+}
