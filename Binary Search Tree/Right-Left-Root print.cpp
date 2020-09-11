530f55c4e8e74e49c177aad5b4168735aa267916
Kaloyan Nikolov <60822878+Kaloyan-Nikolov@users.noreply.github.com>
2020-09-11 10:55:39 +0300

    Create Right-Left-Root print.cpp

---
+++ Binary Search Tree/Right-Left-Root print.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node *leftNode;
+	Node *rightNode;
+	Node(int d)
+	{
+		data = d;
+		leftNode = NULL;
+		rightNode = NULL;
+	}
+};
+
+class Solution
+{
+public:
+	Node* insert(Node* root, int data)
+	{
+		if (root == NULL)
+		{
+			return new Node(data);
+		}
+		else
+		{
+			Node* cur;
+			if (data <= root->data)
+			{
+				cur = insert(root->leftNode, data);
+				root->leftNode = cur;
+			}
+			else
+			{
+				cur = insert(root->rightNode, data);
+				root->rightNode = cur;
+			}
+
+			return root;
+		}
+	}
+
+	void realPrint(Node* curr, bool isRoot)
+	{
+		if (curr == nullptr)
+			return;
+
+		realPrint(curr->rightNode, 0);
+		realPrint(curr->leftNode, 0);
+		cout << curr->data;
+		if (!isRoot)
+			cout << ";";
+	}
+
+	void print(Node *root)
+	{
+		realPrint(root, 1);
+	}
+
+	// SECOND SOLUTION
+
+	/*queue<int> myQ;
+	void realPrint(Node* root)
+	{
+		if (root == nullptr)
+		{
+			return;
+		}
+
+		realPrint(root->rightNode);
+		realPrint(root->leftNode);
+		myQ.push(root->data);
+	}
+
+	void print(Node *root)
+	{
+		realPrint(root);
+		while (myQ.size() > 1)
+		{
+			cout << myQ.front() << ";";
+			myQ.pop();
+		}
+
+		cout << myQ.front();
+	}*/
+};
+
+int main()
+{
+	Solution myTree;
+	Node* root = NULL;
+
+	int t;
+	int data;
+
+	std::cin >> t;
+
+	while (t-- > 0)
+	{
+		std::cin >> data;
+		root = myTree.insert(root, data);
+	}
+
+	myTree.print(root);
+	return 0;
+}
